5f4e39aa6fc0261705d49e809bbea566aac1b66e
Kat Taylor <KathrynTaylor1@BMO.PK5001Z>
2015-06-08 22:26:19 -0700

    version 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,78 @@
 // My Scripts
+
+// Option A: show() and hide() 
+// Option B: toggleClass() or addCLass(), removeClass()
+
+// Click Illustration
+$('button:nth-child(1)').click( function(){
+  
+   // Hide Illustrations 
+  $('.personal').hide();
+  $('.fan').hide();
+  $('.comm').hide();
+  $('.about').hide();
+  // Show Photo
+  $('.AR').show();
+  
+
+});
+
+
+// Click Photo
+$('button:nth-child(2)').click( function(){
+  
+
+   // Hide Illustrations 
+  $('.ar').hide();
+  $('.fan').hide();
+  $('.comm').hide();
+  $('.about').hide();
+  // Show Photo
+  $('.personal').show();
+
+});
+
+// Click Photo
+$('button:nth-child(3)').click( function(){
+  
+
+   // Hide Illustrations 
+  $('.personal').hide();
+  $('.fan').hide();
+  $('.comm').hide();
+  $('.about').hide();
+  // Show Photo
+  $('.fan').show();
+
+});
+
+
+// Click Photo
+$('button:nth-child(4)').click( function(){
+  
+
+   // Hide Illustrations 
+  $('.ar').hide();
+  $('.fan').hide();
+  $('.fan').hide();
+  $('.about').hide();
+  // Show Photo
+  $('.comm').show();
+
+});
+
+// Click Photo
+$('button:nth-child(5)').click( function(){
+  
+
+   // Hide Illustrations 
+  $('.ar').hide();
+  $('.fan').hide();
+  $('.comm').hide();
+  $('.personal').hide();
+  // Show Photo
+  $('.about').show();
+
+});
+
+
